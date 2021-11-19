8e14158a5f933b37be7721706d9cbc91186e1489
hw.park <hw.park@fastfive.co.kr>
2021-11-19 18:27:58 +0900

    message queue 갯수에 따른 로직 분리

--- src/lib/message/index.ts
+++ src/lib/message/index.ts
@@ -13,13 +13,14 @@ const messageController = async () => {
   const queueUrls = getCacheQueueUrls();
 
   if (!_.isEmpty(queueUrls)) {
-    // 여러개의 Message Queue 처리
-    const queueMessages: QueueMessageIE = {};
-
-    for (const queueUrl of queueUrls) {
-      // 한개의 Message Queue당 담고 있는 Message를 담는다.
-      const messages = await getMessageItems(queueUrl);
-      queueMessages[queueUrl] = messages;
+    
+    let queueMessages: QueueMessageIE = {};
+    
+    if (queueUrls.length < 2) {
+      const queueUrl = _.get(queueUrls, 0, "");
+      queueMessages = { ...await singleQueueController(queueUrl) };
+    } else {
+      queueMessages = { ...await multipleQueueController(queueUrls) };
     }
 
     // todo: stateless & statefull 둘 다 로직 짜서 환경변수에 따라 적용하게 하여,
@@ -30,6 +31,29 @@ const messageController = async () => {
     console.log(`time ====> ${new Date().getTime()}`);
     console.log('========================')
   }
+};
+
+// 여러개의 Message Queue 처리
+const multipleQueueController = async (queueUrls: string[]): Promise<QueueMessageIE> => {
+  const queueMessages: QueueMessageIE = {};
+
+  for (const queueUrl of queueUrls) {
+    // 한개의 Message Queue당 담고 있는 Message를 담는다.
+    const messages = await getMessageItems(queueUrl);
+    queueMessages[queueUrl] = messages;
+  }
+
+  return queueMessages;
+};
+
+// 단일 Message Queue 처리
+const singleQueueController = async (queueUrl: string): Promise<QueueMessageIE> => {
+  const queueMessage: QueueMessageIE = {};
+
+  const messages = await getMessageItems(queueUrl);
+  queueMessage[queueUrl] = messages;
+
+  return queueMessage;
 };
 
 export const getMessageItems = async (queueUrl: string): Promise<MessageItems> => {
