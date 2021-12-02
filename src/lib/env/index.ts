ea9dfdee59eea51d0ad43116ec4730bb7d32a706
harry <awakelife93@gmail.com>
2021-12-02 22:23:32 +0900

    환경변수 조건 수정

--- src/lib/env/index.ts
+++ src/lib/env/index.ts
@@ -6,8 +6,7 @@ export default {
   AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY ?? null,
   AWS_SECRET_KEY: process.env.AWS_SECRET_KEY ?? null,
   // * true = socket, false = restful
-  IS_SEND_TO_SOCKET_SUBSCRIBE:
-    process.env.IS_SEND_TO_SOCKET_SUBSCRIBE === "true" ?? true,
+  IS_SEND_TO_SOCKET_SUBSCRIBE: process.env.IS_SEND_TO_SOCKET_SUBSCRIBE ?? true,
   // * 바라보는 Subscribe A 서버 Origin
   SUB_SCRIBE_A_SERVER_ORIGIN:
     process.env.subscribeDomain && process.env.subscribePort
