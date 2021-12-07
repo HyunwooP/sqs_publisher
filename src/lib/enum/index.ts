eb0eaa52a6b663d4447ebf8bfa4e4cb09762174c
hw.park <hw.park@fastfive.co.kr>
2021-12-07 16:52:14 +0900

    enum todo 작성

--- src/lib/enum/index.ts
+++ src/lib/enum/index.ts
@@ -2,6 +2,7 @@ import * as Error from "./error";
 import * as Message from "./message";
 import * as Queue from "./queue";
 
+// todo: enum -> type으로 리팩토링할 수 있는건 리팩토링하기
 export default {
   ...Error,
   ...Message,
